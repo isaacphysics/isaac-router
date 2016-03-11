53b0d2db3993764cac44a36fe00dfd7e208e4405
Ian Davies <git@iandavies.org>
2016-03-11 12:01:01 +0000

    Switch to using docker-compose

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM nginx:1.9.11
-
-COPY nginx.conf /etc/nginx/nginx.conf
--- build-and-run.sh
+++
@@ -1,7 +0,0 @@
-set -e
-docker rm -f router || true
-docker build -t router .
-docker run -p 80:80 --net isaac -d --name router router
-echo Showing router logs. Ctrl-c to detach.
-docker logs -f router
-
