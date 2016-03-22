5a977bece43e8c597ff0dd701651a8268a9baebc
Ian Davies <git@iandavies.org>
2016-03-22 16:27:44 +0000

    Add LetsEncrypt config.

---
+++ renew-certs.sh
@@ -0,0 +1,1 @@
+docker-compose run letsencrypt /letsencrypt/letsencrypt-auto certonly --webroot --webroot-path /var/www --domains dev.isaacphysics.org,staging.isaacphysics.org,test.isaacphysics.org --email isaac-certs@iandavies.org --agree-tos
