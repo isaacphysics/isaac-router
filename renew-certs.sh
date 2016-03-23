0cee0139de083c7d5516976b72828ca639b3b810
Ian Davies <git@iandavies.org>
2016-03-23 11:46:05 +0000

    Allow access from the Cavendish

--- renew-certs.sh
+++ renew-certs.sh
@@ -1,1 +1,7 @@
-docker-compose run letsencrypt /letsencrypt/letsencrypt-auto certonly --webroot --webroot-path /var/www --domains dev.isaacphysics.org,staging.isaacphysics.org,test.isaacphysics.org --email isaac-certs@iandavies.org --agree-tos
+docker-compose run letsencrypt /letsencrypt/letsencrypt-auto \
+  certonly \
+  --webroot \
+  --webroot-path /var/www \
+  --domains live.isaacphysics.org,dev.isaacphysics.org,staging.isaacphysics.org,test.isaacphysics.org \
+  --email isaac-certs@iandavies.org \
+  --agree-tos
