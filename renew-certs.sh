c3fcfcb9794fa267bd842f3e121f1608c33a687b
Ian Davies <git@iandavies.org>
2016-08-01 09:46:29 +0000

    Update letsencrypt email address

--- renew-certs.sh
+++ renew-certs.sh
@@ -3,5 +3,5 @@ docker-compose run letsencrypt /letsencrypt/letsencrypt-auto \
   --webroot \
   --webroot-path /var/www \
   --domains isaacphysics.org,www.isaacphysics.org,live.isaacphysics.org,dev.isaacphysics.org,staging.isaacphysics.org,test.isaacphysics.org,labs.isaacphysics.org \
-  --email isaac-certs@iandavies.org \
+  --email webmaster@isaacphysics.org \
   --agree-tos
