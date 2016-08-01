97ce194c6a604712e0623d7f6cfed0cc62de75d7
Ian Davies <git@iandavies.org>
2016-08-01 09:53:16 +0000

    Split letsencrypt script into generate and renew

---
+++ generate-certs.sh
@@ -0,0 +1,7 @@
+docker-compose run letsencrypt /letsencrypt/letsencrypt-auto \
+  certonly \
+  --webroot \
+  --webroot-path /var/www \
+  --domains isaacphysics.org,www.isaacphysics.org,live.isaacphysics.org,dev.isaacphysics.org,staging.isaacphysics.org,test.isaacphysics.org,labs.isaacphysics.org \
+  --email webmaster@isaacphysics.org \
+  --agree-tos
--- renew-certs.sh
+++ renew-certs.sh
@@ -1,7 +1,6 @@
 docker-compose run letsencrypt /letsencrypt/letsencrypt-auto \
-  certonly \
+  renew \
   --webroot \
   --webroot-path /var/www \
-  --domains isaacphysics.org,www.isaacphysics.org,live.isaacphysics.org,dev.isaacphysics.org,staging.isaacphysics.org,test.isaacphysics.org,labs.isaacphysics.org \
   --email webmaster@isaacphysics.org \
   --agree-tos
