2dec7953e6deccd4d28cf1a54eb11911a0a11289
Ian Davies <git@iandavies.org>
2016-08-22 10:57:09 +0000

    Update config to include content editor

--- generate-certs.sh
+++ generate-certs.sh
@@ -2,6 +2,6 @@ docker-compose run letsencrypt /letsencrypt/letsencrypt-auto \
   certonly \
   --webroot \
   --webroot-path /var/www \
-  --domains isaacphysics.org,www.isaacphysics.org,live.isaacphysics.org,dev.isaacphysics.org,staging.isaacphysics.org,test.isaacphysics.org,labs.isaacphysics.org \
+  --domains isaacphysics.org,www.isaacphysics.org,live.isaacphysics.org,dev.isaacphysics.org,staging.isaacphysics.org,test.isaacphysics.org,labs.isaacphysics.org,tickets.isaacphysics.org,editor.isaacphysics.org \
   --email webmaster@isaacphysics.org \
   --agree-tos
