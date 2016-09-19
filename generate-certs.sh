51760de68e8b8ae5732b01d2f33cfe6dfa4125d5
Ian Davies <git@iandavies.org>
2016-09-19 12:40:32 +0000

    Update certs and nginx config to support docker registry

--- generate-certs.sh
+++ generate-certs.sh
@@ -2,6 +2,6 @@ docker-compose run letsencrypt /letsencrypt/letsencrypt-auto \
   certonly \
   --webroot \
   --webroot-path /var/www \
-  --domains isaacphysics.org,www.isaacphysics.org,live.isaacphysics.org,dev.isaacphysics.org,staging.isaacphysics.org,test.isaacphysics.org,labs.isaacphysics.org,tickets.isaacphysics.org,editor.isaacphysics.org,isaacchemistry.org \
+  --domains isaacphysics.org,www.isaacphysics.org,live.isaacphysics.org,dev.isaacphysics.org,staging.isaacphysics.org,test.isaacphysics.org,labs.isaacphysics.org,tickets.isaacphysics.org,editor.isaacphysics.org,isaacchemistry.org,www.isaacchemistry.org,api.isaacscience.org,docker.isaacscience.org \
   --email webmaster@isaacphysics.org \
   --agree-tos
