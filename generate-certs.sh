17b233488760de1f3928ee1f45094c268f287eec
James Sharkey <jsharkey13@users.noreply.github.com>
2019-04-09 17:36:15 +0100

    Move to a single shared certificate for all domains
    # If this leaks too much information or causes issues, we could try and
    # configure split certs again. SNI is widely supported these days!

--- generate-certs.sh
+++ generate-certs.sh
@@ -1,21 +1,33 @@
-docker-compose run letsencrypt /letsencrypt/letsencrypt-auto \
-  certonly \
-  --webroot \
-  --webroot-path /var/www \
-  --domains \
+docker run --rm -it \
+ -v /local/data/certificates:/etc/letsencrypt -v /local/src/isaac-router/html:/var/www \
+ certbot/certbot certonly --webroot --webroot-path /var/www --domains \
+\
 isaacphysics.org,\
 www.isaacphysics.org,\
 \
+isaaccomputerscience.org,\
+www.isaaccomputerscience.org,\
+\
 live.isaacphysics.org,\
+live.isaaccomputerscience.org,\
 dev.isaacphysics.org,\
+dev.isaaccomputerscience.org,\
 staging.isaacphysics.org,\
+staging.isaaccomputerscience.org,\
 test.isaacphysics.org,\
+test.isaaccomputerscience.org,\
+\
+alpha.isaaccomputerscience.org,\
 \
 labs.isaacphysics.org,\
-tickets.isaacphysics.org,\
-editor.isaacphysics.org,\
-cdn.isaacphysics.org,\
 monitor.isaacphysics.org,\
+\
+tickets.isaacphysics.org,\
+tickets.isaaccomputerscience.org,\
+editor.isaacphysics.org,\
+editor.isaaccomputerscience.org,\
+cdn.isaacphysics.org,\
+cdn.isaaccomputerscience.org,\
 \
 isaacchemistry.org,\
 www.isaacchemistry.org,\
@@ -28,10 +40,9 @@ www.isaacbooks.org,\
 www.cavendish-quantum.org.uk,\
 \
 api.isaacscience.org,\
-\
 docker.isaacscience.org,\
-\
 owncloud.isaacscience.org\
+\
   --email webmaster@isaacphysics.org \
   --no-eff-email \
   --agree-tos
