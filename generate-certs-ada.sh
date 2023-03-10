f9b371b2ce853c0d1669a874584eac9b675f1be7
Matthew Trew <matthew.trew@raspberrypi.org>
2023-03-10 11:43:17 +0000

    Add Ada cert generation script

---
+++ generate-certs-ada.sh
@@ -0,0 +1,8 @@
+# All Ada domains (live, staging etc.):
+docker run --rm -it \
+ -v /local/data/cloudflare-dns:/etc/cloudflare-dns \
+ -v /local/data/certificates:/etc/letsencrypt \
+ certbot/dns-cloudflare certonly \
+  --dns-cloudflare \
+  --dns-cloudflare-credentials /etc/cloudflare-dns.ini \
+  -d *.adacomputerscience.org
