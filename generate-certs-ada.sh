db6f8d5b3e41fdcf433a5550a1d1183a036ed46d
James Sharkey <jsharkey13@users.noreply.github.com>
2023-03-28 16:14:28 +0100

    Add short URL domain and code editor for Ada

--- generate-certs-ada.sh
+++ generate-certs-ada.sh
@@ -7,3 +7,16 @@ docker run --rm -it \
   --dns-cloudflare-credentials /etc/cloudflare-dns/cloudflare-api-token.ini \
   -d adacomputerscience.org \
   -d *.adacomputerscience.org
+
+# A temporary certificate for the short Ada domain:
+# docker run --rm -it \
+#  -v /local/data/certificates:/etc/letsencrypt -v /local/src/isaac-router/html:/var/www \
+#  certbot/certbot certonly --webroot --webroot-path /var/www --domains \
+# \
+# ada-cs.org,\
+# code-editor.ada-cs.org\
+# \
+#   --cert-name ada-cs.org \
+#   --email webmaster@isaacphysics.org \
+#   --no-eff-email \
+#   --agree-tos
